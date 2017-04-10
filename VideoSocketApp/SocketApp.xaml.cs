5bc45de18b450b1594d568579816c514595784c6
Jorge Perales Diaz <slipknot_jpd@hotmail.com>
2017-04-09 21:38:57 -0500

    VideoSocketApp
    use of advanced streaming libraries

---
+++ VideoSocketApp/SocketApp.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace VideoSocketApp
+{
+    public sealed partial class MainPage : Page
+    {
+        private Guid _guid = Guid.NewGuid();
+        private MediaPlaybackList _playlist = null;
+        private bool Buffering => _playlist.Items.Count == 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Media.MediaEnded += Media_MediaEnded;
+
+            _playlist = new MediaPlaybackList();
+            _playlist.CurrentItemChanged += (sender, args) => _playlist.Items.Remove(args.OldItem);
+            _playlist.ItemOpened += Playlist_ItemOpened;
+            _playlist.ItemFailed += Playlist_ItemFailed;
+            Media.SetPlaybackSource(_playlist);
+        }
+
+        private void Playlist_ItemOpened(MediaPlaybackList sender, MediaPlaybackItemOpenedEventArgs args)
+        {
+            Debug.WriteLine("New playlist item Opened");
+        }
+
+        private void Playlist_ItemFailed(MediaPlaybackList sender, MediaPlaybackItemFailedEventArgs args)
+        {
+            Debug.WriteLine("New playlist item Failed!");
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            await Task.WhenAll(
+                PlayNextVideo(),
+                DownloadVideos());
+        }
+
+        private async Task DownloadVideos()
+        {
+            var socket = new StreamSocket();
+            while (true)
+            {
+                try
+                {
+                    await socket.ConnectAsync(new HostName("192.168.56.1"), "13337");
+                    break;
+                }
+                catch { }
+            }
+            byte[] inbuffer = new byte[1];
+            IBuffer result = await socket.InputStream.ReadAsync(inbuffer.AsBuffer(), inbuffer.AsBuffer().Capacity, InputStreamOptions.None);
+            while (true)
+            {
+                Debug.WriteLine($"Requesting next download ({_guid})");
+                IBuffer outbuffer = Encoding.UTF8.GetBytes($"{_guid}").AsBuffer();
+                await socket.OutputStream.WriteAsync(outbuffer);
+                inbuffer = new byte[10000000];
+                result = await socket.InputStream.ReadAsync(inbuffer.AsBuffer(), inbuffer.AsBuffer().Capacity, InputStreamOptions.Partial);
+                Debug.WriteLine($"Download complete: {result.Length} bytes");
+                byte[] guid = result.ToArray().Take(16).ToArray();
+                byte[] data = result.ToArray().Skip(16).ToArray();
+                _guid = new Guid(guid);
+                var stream = new MemoryStream(data);
+                var source = MediaSource.CreateFromStream(stream.AsRandomAccessStream(), "video/mp4");
+                var item = new MediaPlaybackItem(source);
+                _playlist.Items.Add(item);
+                Debug.WriteLine($"New playlist item added to list: {data.Length} bytes");
+                Debug.WriteLine($"Playlist now contains {_playlist.Items.Count} items");
+                if (_playlist.Items.Count > 2)
+                {
+                    Debug.WriteLine("Playlist stuck, moving next");
+                    _playlist.MoveNext();
+                }
+                Debug.WriteLine($"Media state: {Media.CurrentState}");
+                if (Media.CurrentState != MediaElementState.Playing)
+                {
+                    Debug.WriteLine("Playing...");
+                    Media.Play();
+                }
+                inbuffer = new byte[10000000];
+            }
+        }
+
+        private async Task PlayNextVideo()
+        {
+            Debug.WriteLine($"Playing next video...");
+            while (true)
+            {
+                if (!Buffering)
+                {
+                    BufferingLbl.Visibility = Visibility.Collapsed;
+                    Media.Play();
+                    break;
+                }
+                else
+                {
+                    Debug.WriteLine($"Buffering...");
+                    BufferingLbl.Visibility = Visibility.Visible;
+                    await Task.Delay(500);
+                }
+            }
+        }
+
+        private async void Media_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"Playback ended");
+            await PlayNextVideo();
+        }
+    }
+}
---
+++ VideoSocketAppServer/SocketServer.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Networking;
+using Windows.Networking.Connectivity;
+using Windows.Networking.Sockets;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace VideoSocketAppServer
+{
+    public sealed partial class SocketAppServer : Page
+    {
+        private int _port = 13337;
+        private MediaCapture _mediaCap;
+        private StreamSocketListener _listener;
+        private ManualResetEvent _signal = new ManualResetEvent(false);
+        private List<Connection> _connections = new List<Connection>();
+        internal CurrentVideo CurrentVideo = new CurrentVideo();
+        public SocketAppServer()
+        {
+            InitializeComponent();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await InitialiseVideo();
+            await StartListener();
+            await BeginRecording();
+        }
+        private async Task InitialiseVideo()
+        {
+            Debug.WriteLine($"Initialising video...");
+            var settings = ApplicationData.Current.LocalSettings;
+            string preferredDeviceName = $"{settings.Values["PreferredDeviceName"]}";
+            if (string.IsNullOrWhiteSpace(preferredDeviceName))
+                preferredDeviceName = "Microsoft® LifeCam HD-3000";
+            var videoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+            DeviceInformation device = videoDevices.FirstOrDefault(x => x.Name == preferredDeviceName);
+            if (device == null)
+                device = videoDevices.FirstOrDefault();
+            if (device == null)
+                throw new Exception("Cannot find a camera device");
+            else
+            {
+                _mediaCap = new MediaCapture();
+                var initSettings = new MediaCaptureInitializationSettings { VideoDeviceId = device.Id };
+                await _mediaCap.InitializeAsync(initSettings);
+                _mediaCap.Failed += new MediaCaptureFailedEventHandler(MediaCaptureFailed);
+            }
+            Debug.WriteLine($"Video initialised");
+        }
+
+        private void MediaCaptureFailed(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
+        {
+            Debug.WriteLine($"Video capture failed: {errorEventArgs.Message}");
+        }
+
+        private async Task BeginRecording()
+        {
+            while (true)
+            {
+                try
+                {
+                    Debug.WriteLine($"Recording started");
+                    var memoryStream = new InMemoryRandomAccessStream();
+                    await _mediaCap.StartRecordToStreamAsync(MediaEncodingProfile.CreateMp4(VideoEncodingQuality.Vga), memoryStream);
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+                    await _mediaCap.StopRecordAsync();
+                    Debug.WriteLine($"Recording finished, {memoryStream.Size} bytes");
+                    memoryStream.Seek(0);
+                    CurrentVideo.Id = Guid.NewGuid();
+                    CurrentVideo.Data = new byte[memoryStream.Size];
+                    await memoryStream.ReadAsync(CurrentVideo.Data.AsBuffer(), (uint)memoryStream.Size, InputStreamOptions.None);
+                    Debug.WriteLine($"Bytes written to stream");
+                    _signal.Set();
+                    _signal.Reset();
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"StartRecording -> {ex.Message}");
+                    break;
+                }
+            }
+        }
+
+        private async Task StartListener()
+        {
+            Debug.WriteLine($"Starting listener");
+            _listener = new StreamSocketListener();
+            _listener.ConnectionReceived += (sender, args) =>
+            {
+                Debug.WriteLine($"Connection received from {args.Socket.Information.RemoteAddress}");
+                _connections.Add(new Connection(args.Socket, this));
+            };
+            HostName host = NetworkInformation.GetHostNames().FirstOrDefault(x => x.IPInformation != null && x.Type == HostNameType.Ipv4);
+            await _listener.BindEndpointAsync(host, $"{_port}");
+            Debug.WriteLine($"Listener started on {host.DisplayName}:{_listener.Information.LocalPort}");
+        }
+
+        internal byte[] GetCurrentVideoDataAsync(Guid guid)
+        {
+            if (CurrentVideo.Id == Guid.Empty || CurrentVideo.Id == guid)
+                 _signal.WaitOne();
+            return CurrentVideo.Id.ToByteArray().Concat(CurrentVideo.Data).ToArray();
+        }
+    }
+}
